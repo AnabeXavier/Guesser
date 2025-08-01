37dece2d10e93043117c584b09fa60fb6bec8e4a
anaxavier <xavier.anabe@gmail.com>
2025-08-01 18:10:23 -0300

    Alterações seguindo os requisitos do exercício.

--- guesser.js
+++ guesser.js
@@ -1,14 +1,34 @@
+// Gera número secreto com math.random
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;  
+let tentativas = 0;
+const maxTentativas = 10;
+
+// Chama a função
 function resultadoNumero(){
     const numeroUsuario = Number(document.getElementById('numero').value);
-    let numeroSecreto = 71;
     const mensagem = document.getElementById('mensagem');
 
+// Verifica se o número é válido
+     if (isNaN(numeroUsuario) || numeroUsuario < 1 || numeroUsuario > 100) {
+        mensagem.textContent = "Por favor, digite um número entre 1 e 100.";
+        return;
+    }
+    tentativas++;
+
+// Verifica o número de tentativas
+     if (tentativas >= maxTentativas) {
+        mensagem.textContent = `Desculpa, você atingiu o número máximo de ${maxTentativas} tentativas. O número secreto era ${numeroSecreto}`;
+        document.getElementById('numero').disabled = true;
+        return;
+    }
+
+// Conta o número de tentativas e revela resultado
     if (numeroUsuario < numeroSecreto) {
-        mensagem.textContent = "ops, o número secreto é mais alto";
+        mensagem.textContent = `tentativas ${tentativas} ops, o número secreto é mais alto`;
     } else if (numeroUsuario > numeroSecreto) {
-        mensagem.textContent = "opa, o número secreto é mais baixo";
+        mensagem.textContent = `tentativas ${tentativas} opa, o número secreto é mais baixo`;
     } else {
-        mensagem.textContent = "Muito bem, você acertou!";
+        mensagem.textContent = `tentativas ${tentativas} Muito bem, você acertou!`;
     }
 
 }
